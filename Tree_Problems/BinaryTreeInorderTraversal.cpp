f28c93b7de505b76d0f1e5b5b4949a49950ac57d
Harpreet <harkhush1102@gmail.com>
2021-12-20 00:53:01 +0530

    added iterative approach(using stack) also in inorder traversal of binary tree

--- Tree_Problems/BinaryTreeInorderTraversal.cpp
+++ Tree_Problems/BinaryTreeInorderTraversal.cpp
@@ -2,6 +2,7 @@
 
 // Given the root of a binary tree, return the inorder traversal of its nodes' values.
 
+// 1. RECURSIVE APPROACH
 class Solution
 {
 public:
@@ -14,5 +15,41 @@ public:
         v.push_back(root->val);
         inorderTraversal(root->right);
         return v;
+    }
+};
+
+// 2. ITERATIVE APPROACH(USING STACK)
+class Solution
+{
+public:
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> res;
+        iterative(root, res);
+        return res;
+    }
+    void iterative(TreeNode *root, vector<int> &res)
+    {
+        stack<TreeNode *> s;
+
+        while (root || !s.empty())
+        {
+            //keep moving towards left
+            while (root)
+            {
+                s.push(root);
+                root = root->left;
+            }
+
+            //backtrack and pick up most recent element
+            root = s.top();
+            s.pop();
+
+            //push recent element in res
+            res.push_back(root->val);
+
+            //move towards right
+            root = root->right;
+        }
     }
 };
